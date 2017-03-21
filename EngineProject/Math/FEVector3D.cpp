ff94efbb8d85dc527b014d10991159e10c8a472e
B.F. Peng <B.F. Peng>
2017-03-22 00:35:31 +0800

    commit

--- EngineProject/Math/FEVector3D.cpp
+++ EngineProject/Math/FEVector3D.cpp
@@ -1,61 +1,89 @@
-#include "Include\FEVector3D.h"
+#include "Include/FEVector3D.h"
+#include "Include/FEConstant.h"
 #include <cmath>
 
-namespace FunnyEngine::Math
+namespace FunnyEngine
 {
-	FEVector3D::FEVector3D(real x, real y, real z, real w)
-		:_x(x), _y(y), _z(z), _w(w)
+	namespace Math
 	{
-	}
-
-	FEVector3D::FEVector3D(real x, real y, real z)
-		: _x(x), _y(y), _z(z), _w(0)
-	{
-	}
-
-	FEVector3D::FEVector3D()
-		: _x(0), _y(0), _z(0), _w(0)
-	{
-	}
-
-	real FEVector3D::magnitude() const
-	{
-		return std::sqrt(_x*_x + _y*_y + _z*_z);
-	}
-
-	real FEVector3D::magnitudeSquared() const
-	{
-		return _x*_x + _y*_y + _z*_z;
-	}
-
-	real FEVector3D::distanceTo(const FEVector3D & other) const
-	{
-		return std::sqrt(
-			(_x - other._x) * (_x - other._x) +
-			(_y - other._y) * (_y - other._y) +
-			(_z - other._z) * (_z - other._z)
-		);
-	}
-
-	real FEVector3D::dot(const FEVector3D & other)
-	{
-		return _x * other._x + _y * other._y + _z + other._z;
-	}
-
-	FEVector3D & FEVector3D::cross(const FEVector3D & other)
-	{
-		return FEVector3D((_y * other._z) - (_z * other._y),
-			(_z * other._x) - (_x * other._z),
-			(_x * other._y) - (_y * other._x));
-	}
-
-	FEVector3D & FEVector3D::normalized()
-	{
-		real length = magnitude();
-		if (length < 0.0000001)
+		FEVector3D::FEVector3D(real _x, real _y, real _z)
+			: x(_x), y(_y), z(_z), w(1)
 		{
-			return zero();
 		}
-		return FEVector3D(_x / length, _y / length, _z / length);
+
+		FEVector3D::FEVector3D()
+			: x(0), y(0), z(0), w(1)
+		{
+		}
+
+		real FEVector3D::magnitude() const
+		{
+			return std::sqrt(x*x + y*y + z*z);
+		}
+
+		real FEVector3D::magnitudeSquared() const
+		{
+			return x*x + y*y + z*z;
+		}
+
+		real FEVector3D::distanceTo(const FEVector3D & other) const
+		{
+			return std::sqrt(
+				(x - other.x) * (x - other.x) +
+				(y - other.y) * (y - other.y) +
+				(z - other.z) * (z - other.z)
+			);
+		}
+
+		real FEVector3D::dot(const FEVector3D & other)
+		{
+			return x * other.x + y * other.y + z + other.z;
+		}
+
+		FEVector3D FEVector3D::cross(const FEVector3D & other)
+		{
+			return FEVector3D((y * other.z) - (z * other.y),
+				(z * other.x) - (x * other.z),
+				(x * other.y) - (y * other.x));
+		}
+
+		FEVector3D FEVector3D::normalized()
+		{
+			real length = magnitude();
+			if (length < EPSILON)
+			{
+				return zero();
+			}
+			return FEVector3D(x / length, y / length, z / length);
+		}
+
+		FEVector3D FEVector3D::operator+(FEVector3D & other)
+		{
+			return FEVector3D(x + other.x, y + other.y, z + other.z);
+		}
+
+		FEVector3D FEVector3D::operator-(FEVector3D & other)
+		{
+			return FEVector3D(x - other.x, y - other.y, z - other.z);
+		}
+
+		FEVector3D FEVector3D::operator*(real ratio)
+		{
+			return FEVector3D(x * ratio, y * ratio, z * ratio);
+		}
+
+		void FEVector3D::operator+=(FEVector3D & other)
+		{
+			x += other.x;
+			y += other.y;
+			z += other.z;
+		}
+
+		void FEVector3D::operator-=(FEVector3D & other)
+		{
+			x -= other.x;
+			y -= other.y;
+			z -= other.z;
+		}
 	}
 }
--- EngineProject/Math/Include/FEMatrix3D.h
+++ EngineProject/Math/Include/FEMatrix3D.h
@@ -1,13 +1,15 @@
 #ifndef __FE_MATRIX3D_H__
 #define __FE_MATRIX3D_H__
 
+#include "FEVector3D.h"
+
 namespace FunnyEngine
 {
 	namespace Math
 	{
 		class FEMatrix3D
 		{
-
+			
 		};
 	}
 }
